2a3b03f5f9156d35be3ae2dca17f3f49b4f9e284
Brittni Watkins <blwatkins@smu.edu>
2018-01-01 16:51:46 -0600

    Add file for hsb color functions. Complete rainbow sine curve visualization. Add function for creating curves and instructions.

--- docs/RainbowSineCurves/rainbow_sine_curves.js
+++ docs/RainbowSineCurves/rainbow_sine_curves.js
@@ -1,33 +1,15 @@
 // Rainbow Sine Curve Visualization
 // Press the 'a' key to change the background from black to white
 
-//ArrayList<Curve> curves = new ArrayList<Curve>();
-
-var xStart = 10;
-var amp = 10;
-var currentY = 0;
-var yBuffer = 10;
-var xBuffer = 5;
+var curves = [];
 var blackBg;
-
-var p;
 
 function setup() {
     createCanvas(windowWidth-20, windowHeight-20);
     colorMode(HSB, 360);
     blackBg = true;
-    var numPoints = parseInt(random(10, 100)); 
-
-    p = new Point(100, 200, 0.6, 50);
-    print(p);
-
-    // while (currentY < height) {
-    //   amp = random(10, 50);
-    //   Curve c = new Curve(xStart, currentY + amp + yBuffer, width - (xBuffer * 4), amp, (int)random(1, 5), numPoints);
-    //   c.setCurveCol(getRandomColType());
-    //   curves.add(c);
-    //   currentY = currentY + (amp * 2) + yBuffer;
-    // }
+    createCurves();
+    createInstructions();
 }
 
 function draw() {
@@ -41,13 +23,7 @@ function draw() {
 
     rect(-10, -10, width+10, height+10);
 
-    p.display();
-    p.move();
-
-    // for (Curve c : curves) {
-    //   c.moveCurve();
-    //   c.displayCurve();
-    // }
+    curves.forEach(displayAndMoveCurve);
 }
 
 function keyTyped() {
@@ -56,4 +32,36 @@ function keyTyped() {
       blackBg = !blackBg;
     }
 
+}
+
+function createInstructions() {
+    var instructionHeader = createElement("h1", "Instructions");
+    var instructionList = createElement("ul", "");
+    var instruction = createElement("li", "Press the 'a' key to change the background to black or white");
+    instruction.parent(instructionList);
+}
+
+function createCurves() {
+    var numPoints = parseInt(random(10, 100)); 
+    var xStart = 10;
+    var currentY = 0;
+    var yBuffer = 10;
+    var xBuffer = 5;
+    var amp = 10;
+    var frequency = 1;
+
+    while (currentY < height) {
+      amp = random(10, 50);
+      frequency = parseInt(random(1, 5));
+      var c = new Curve(xStart, currentY + amp + yBuffer, width - (xBuffer * 4), amp, frequency, numPoints);
+      c.setColor(getRandomColorType());
+      curves.push(c);
+      currentY = currentY + (amp * 2) + yBuffer;
+    }
+
+}
+
+function displayAndMoveCurve(curve) {
+    curve.display();
+    curve.move();
 }
